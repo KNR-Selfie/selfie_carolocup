018038280ddd44691f3c31bf2e30eda0ca8df2a2
nels3 <kornelialukojc@gmail.com>
2019-02-03 15:13:17 +0100

    More tryouts

--- src/selfie_logic/scripts/ChangeLaneClass.py
+++ src/selfie_logic/scripts/ChangeLaneClass.py
@@ -19,17 +19,23 @@ class ChangeLaneClass:
         self.r_poly = Polynomial(0)
         self.l_poly = Polynomial(0)
 
-        self.lookahead_c = 0.0
+        self.lookahead_c = 0.2
         self.lookahead_f = 0.3
-        self.lookahead_max = 0.5
+        self.lookahead_max = 0.8
         #width of road
-        self.width = 0.0
+        self.width = 0.45
 
         #obstacles polygon
         self.polygon = PolygonStamped()
 
         #points on current lane
         self.points_on_lane = 0
+        self.counter = 0
+
+        self.start_maneuver = 0
+        self.start_distance = 0
+        self.obstacle_on_right = 0
+        self.changed = 0
 
     def get_offset(self, lookahead, poly):
         '''
@@ -53,51 +59,112 @@ class ChangeLaneClass:
             c_dist = -c_dist
 
         return c_dist
+        
 
-    def check_polygon(self):
+    
+    def check_polygon_two_lines_four_points(self):
         '''
         Method that checks if points of polygon are inside current lane
         '''
         #check offset of middle lane
         c_dis_c = self.get_offset(self.lookahead_c,self.c_poly)
         points_tmp = 0
-
         #we are on right so we check right lane
         if c_dis_c<0:
             self.right_lane = 1
+            
             for i in range(0,4):
                 #get x_coor with shift
                 x_coor = self.polygon.polygon.points[i].x + 0.2
-                #check if x_coor is close enough
-                if x_coor> 0.3 and x_coor<self.lookahead_max:
-                    #get y_coor 
-                    y_coor = -self.polygon.polygon.points[i].y
-                    #get y_coor on right and center line with margin
-                    c_dis_f = self.get_offset(x_coor, self.c_poly)-0.1
-                    r_dis_f = self.get_offset(x_coor, self.r_poly)+0.1
-                    #checkout width 
-                    self.width = -c_dis_f + r_dis_f
-                    #if y_coor is beetween line add point
-                    if c_dis_f<y_coor and y_coor<r_dis_f:
-                        points_tmp +=1
-                
-                    
-        else:   
-            self.right_lane = 0   
-            for i in range(0,4):
-                x_coor = self.polygon.polygon.points[i].x + 0.2
-                if x_coor> 0.3 and x_coor<self.lookahead_max:
-                    y_coor = -self.polygon.polygon.points[i].y
-                    c_dis_f = self.get_offset(x_coor, self.c_poly)+0.1
-                    l_dis_f = self.get_offset(x_coor, self.l_poly)-0.1
-                    self.width = +c_dis_f + -l_dis_f
-                    if c_dis_f>y_coor and y_coor>l_dis_f:
-                        points_tmp +=1
-
-                    
+                #get y_coor with minus sign
+                y_coor = -self.polygon.polygon.points[i].y
+                            
+                #get y_coor on right and center line with margin
+                c_dis_f = self.get_offset(x_coor, self.c_poly)-0.1
+                r_dis_f = self.get_offset(x_coor, self.r_poly)+0.1
+                               
+                if y_coor < r_dis_f and c_dis_f< y_coor and x_coor> 0.3 and x_coor< self.lookahead_max:
+                    points_tmp +=1
+                    #rospy.loginfo("X: %f C:%f Y: %f R: %f",x_coor,c_dis_f, y_coor,r_dis_f)               
+        
+        else:
+            self.right_lane = 0                      
+        
         #return box only if more points are inside
-        #if points_tmp >1:
-        self.points_on_lane = points_tmp
+        if points_tmp >1:
+            self.points_on_lane += points_tmp
+
+    def check_polygon_two_lines_middle_four_points(self):
+        '''
+        Method that checks if middle points of polygon are inside current lane
+        '''
+        #check offset of middle lane
+        c_dis_c = self.get_offset(self.lookahead_c,self.c_poly)
+        points_tmp = 0
+        #we are on right so we check right lane
+        if c_dis_c<0:
+            self.right_lane = 1
+            x_middle =0
+            y_middle = 0
+            for i in range(0,4):
+                #get x_coor with shift
+                x_coor = self.polygon.polygon.points[i].x + 0.2
+                #get y_coor with minus sign
+                y_coor = -self.polygon.polygon.points[i].y
+
+                x_middle +=x_coor
+                y_middle +=y_coor
+                #get x_coor with shift
+                x_coor = self.polygon.polygon.points[i].x + 0.2
+                #get y_coor with minus sign
+                y_coor = -self.polygon.polygon.points[i].y
+
+            x_middle /=4
+            y_middle /=4
+            x_min = min(self.polygon.polygon.points[0].x + 0.2,self.polygon.polygon.points[1].x + 0.2,self.polygon.polygon.points[2].x + 0.2,self.polygon.polygon.points[3].x + 0.2)
+            y_min = min(-self.polygon.polygon.points[0].y,-self.polygon.polygon.points[1].y,-self.polygon.polygon.points[2].y,-self.polygon.polygon.points[3].y)
+            y_max = max(-self.polygon.polygon.points[0].y,-self.polygon.polygon.points[1].y,-self.polygon.polygon.points[2].y,-self.polygon.polygon.points[3].y)
+            y_wid = (y_max-y_min)/2
+
+            #get y_coor on right and center line with margin
+            c_dis_m = self.get_offset(x_middle, self.c_poly)
+            r_dis_m = self.get_offset(x_middle, self.r_poly)
+                            
+            if c_dis_m<y_middle and y_middle<r_dis_m and x_min>0.3 and x_min<0.7:
+                points_tmp +=1      
+        
+        else:
+            self.right_lane = 0                  
+        
+        #return box only if more points are inside
+        if points_tmp >0:
+            self.points_on_lane += points_tmp
+
+    def check_polygon_maneuver(self):
+        #check offset of middle lane
+        c_dis_c = self.get_offset(self.lookahead_c,self.c_poly)
+        #we are on right so we check right lane
+        if c_dis_c<0:
+            self.right_lane = 1
+        else:
+            self.right_lane = 0
+
+        for i in range(0,4):
+            #get x_coor with shift
+            x_coor = self.polygon.polygon.points[i].x + 0.2
+            #get y_coor with minus sign
+            y_coor = -self.polygon.polygon.points[i].y
+
+            if y_coor<0 and y_coor>-0.5 and x_coor <0.5 and x_coor > 0.1:
+                self.obstacle_on_right +=1
 
         
-
+    def get_area(self):
+        X = [self.polygon.polygon.points[0].x,self.polygon.polygon.points[1].x,self.polygon.polygon.points[2].x,self.polygon.polygon.points[3].x]
+        Y = [self.polygon.polygon.points[0].y,self.polygon.polygon.points[1].y,self.polygon.polygon.points[2].y,self.polygon.polygon.points[3].y]
+        area = 0
+        j = 4 - 1
+        for i in range(0,3):
+            area += (X[j]+X[i])*(Y[j]-Y[i])
+            j = i
+        return (abs(area/2.0))
--- src/selfie_logic/scripts/change_lane_logic.py
+++ src/selfie_logic/scripts/change_lane_logic.py
@@ -24,17 +24,54 @@ def distance_callback(msg):
   #save distance from stm32
   CLC.distance = msg.data
   #showing variables on screen
-  rospy.loginfo("Points: %d \t Lane: %d", CLC.points_on_lane, CLC.right_lane)
+  #rospy.loginfo("Points: %d \t Lane: %d", CLC.points_on_lane, CLC.right_lane)
 
 def obstacles_callback(msg):
   CLC.points_on_lane = 0
+  #if we are on right lane and want to detect obstacle
+  if CLC.start_maneuver == 0:
+    for box_nr in range (len(msg.polygons)-1, 0, -1):    
+      CLC.polygon.polygon = msg.polygons[box_nr]
+      #check each polygon
+      CLC.check_polygon_two_lines_middle_four_points()
+  
+    if CLC.points_on_lane>0:
+      CLC.counter +=1
+      if CLC.counter > 5:
+        CLC.counter = 5
+    else:
+      CLC.counter -=1
+      if CLC.counter<0:
+        CLC.counter = 0
 
-  for box_nr in range (len(msg.polygons)-1, 0, -1):    
-    CLC.polygon.polygon = msg.polygons[box_nr]
-    #check each polygon
-    CLC.check_polygon()
-  
-  
+    if CLC.counter>1:
+      CLC.start_maneuver = 1
+      CLC.start_distance = CLC.distance
+      CLC.counter = 0
+      CLC.changed = 0
+
+  elif CLC.start_maneuver ==1:
+    CLC.obstacle_on_right = 0
+    for box_nr in range (len(msg.polygons)-1, 0, -1):    
+      CLC.polygon.polygon = msg.polygons[box_nr]
+      #check each polygon
+      CLC.check_polygon_maneuver()
+    if CLC.obstacle_on_right > 0:
+      CLC.counter +=1
+      if CLC.counter > 5:
+        CLC.counter = 5
+    else:
+      CLC.counter -=1
+      if CLC.counter<0:
+        CLC.counter = 0
+    if CLC.counter>3:
+      CLC.changed = 1
+    if CLC.changed == 1 and CLC.right_lane ==1:
+      CLC.start_maneuver = 0
+    
+    
+
+    
 if __name__ == '__main__':
     rospy.init_node('change_lane_logic')
 
@@ -44,11 +81,13 @@ if __name__ == '__main__':
     
     speed_logic_pub = rospy.Publisher("speed_logic", Float32, queue_size=1)
 
-    rate = rospy.Rate(50)
-  
     
     while not rospy.is_shutdown():
-      #rospy.loginfo("Points: %d \t Lane: %d", CLC.points_on_lane, CLC.right_lane)
-      #rospy.loginfo(CLC.distance, CLC.right_lane)
-      #rate.sleep()
-      rospy.spin()
+      if CLC.right_lane ==1:
+        rospy.loginfo("R. %d %d ", CLC.start_maneuver, CLC.obstacle_on_right)
+      else CLS.right_lane ==0:
+        rospy.loginfo("L. %d %d ", CLC.start_maneuver, CLC.obstacle_on_right)
+
+
+      rospy.sleep(0.1)
+      #rospy.spin()
